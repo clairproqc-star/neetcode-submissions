2d04d6e85f69519ab3696eed15fc3554ed540a6b
clairproqc-star <clairpro.qc@gmail.com>
2026-08-23 12:35:16 -0400

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        answerMap={
+            ")":"(",
+            "}":"{",
+            "]":"["
+        }
+        window=[]
+        for r in range(len(s)):
+            if len(window)>0 and answerMap.get(s[r],0)==window[-1]:
+                print('yes')
+                window.pop()
+              
+            else:
+                window.append(s[r])
+        return len(window)==0  
